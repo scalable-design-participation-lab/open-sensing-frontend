1dfcb43bb7c1411b4a6bb8a506ac031d8586a0f5
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2023-08-30 16:43:57 -0400

    feat(element-plus)

--- components/SidebarNav.vue
+++ components/SidebarNav.vue
@@ -1,10 +1,32 @@
 <script setup>
+import { ElDropdown, ElCollapse, ElCollapseItem } from 'element-plus'
+import { ArrowDown } from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+
 const props = defineProps({
   location: { type: String, default: () => '' },
 })
 </script>
 
 <template>
+  <el-dropdown>
+    <span class="el-dropdown-link">
+      Dropdown List
+      <el-icon class="el-icon--right">
+        <arrow-down />
+      </el-icon>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>Action 1</el-dropdown-item>
+        <el-dropdown-item>Action 2</el-dropdown-item>
+        <el-dropdown-item>Action 3</el-dropdown-item>
+        <el-dropdown-item disabled>Action 4</el-dropdown-item>
+        <el-dropdown-item divided>Action 5</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+
   <div
     class="absolute bg-white/90 px-3 py-3 w-1/6 overflow-y-auto h-[calc(100vh-5rem)]"
     style="background-color: #026341"
@@ -70,4 +92,11 @@ const props = defineProps({
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
+</style>
