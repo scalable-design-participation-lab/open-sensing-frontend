d0e083f7ea51e1f71d46cf72da4563073a9810cf
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2023-10-19 22:12:06 -0400

    feat(Dashboard): Added sensor charts and data

---
+++ components/SensorDashboard/DataCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { LineChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
+
+// set up props for the component
+const props = defineProps({
+  // make chartData an object and give it a default value
+  chartData: { type: Object, default: () => {} },
+})
+</script>
+
+<template>
+  <!-- <div id="overview-card" class="w-96 h-52 p-4 bg-white"> -->
+  <div>
+    <LineChart :chart-data="chartData" />
+  </div>
+  <!-- </div> -->
+</template>
+
+<style>
+#overview-card {
+  bottom: 1.25rem;
+  right: 1.25rem;
+  border-radius: 1.5rem;
+}
+
+.table {
+  font-size: 1.125rem; /* 18px */
+  line-height: 1.75rem; /* 28px */
+}
+
+#text-right {
+  text-align: right;
+}
+</style>
--- components/SensorDashboard/index.vue
+++ components/SensorDashboard/index.vue
@@ -1,5 +1,40 @@
 <script setup>
 import { ref } from 'vue'
+import { csv } from 'd3'
+
+const chartData = ref({})
+
+onMounted(async () => {
+  const allData = await csv('/sensorData.csv')
+  const labels = allData.map((d) => d['timestamp'])
+  // loop through all the elements in metrics
+  metrics.forEach((metric) => {
+    chartData.value[metric] = {
+      labels,
+      datasets: [
+        {
+          layout: { padding: 50 },
+          data:
+            metric != 'heat_index'
+              ? allData.map((d) => parseFloat(d[metric]))
+              : allData.map(
+                  (d) =>
+                    0.5 *
+                    (parseFloat(d['temperature']) +
+                      61 +
+                      (parseFloat(d['temperature']) - 68) * 1.2 +
+                      parseFloat(d['relative_humidity']) * 0.094)
+                ),
+          fill: false,
+          pointStyle: false,
+          borderColor: 'rgb(255, 255, 255)',
+          tension: 0.1,
+          // options: { plugins: { legend: { position: 'right' } } },
+        },
+      ],
+    }
+  })
+})
 
 const devNames = ref([
   'HOWARD',
@@ -9,12 +44,28 @@ const devNames = ref([
   'FOREST',
   "MARINER'S HARBOR",
 ])
+
+const metrics = [
+  'temperature',
+  'heat_index',
+  'relative_humidity',
+  'pm25',
+  'pm10',
+]
+
+const getClass = (metric) => {
+  {
+    const tailwindClass = 'mt-8 bg-[#41715C] overflow-x-scroll'
+    if (metric != 'temperature') return tailwindClass + ' w-1/2'
+    else return tailwindClass
+  }
+}
 </script>
 
 <template>
   <div
-    class="w-full px-4 bg-[#004E32]"
-    :style="{ height: 'calc(100vh - 50px);' }"
+    class="w-full px-4 bg-[#004E32] overflow-x-scroll"
+    :style="{ height: 'calc(100vh - 50px)' }"
   >
     <div class="bg-[#41715C] overflow-x-scroll">
       <h1 class="p-4">Overview</h1>
@@ -27,8 +78,13 @@ const devNames = ref([
       </table>
     </div>
 
-    <div class="mt-8 bg-[#41715C] overflow-x-scroll">
-      <h1 class="p-4">Temperature</h1>
+    <div
+      v-for="(metricData, metric) in chartData"
+      :key="metric"
+      :class="getClass(metric)"
+    >
+      <h1 class="p-4">{{ metric }}</h1>
+      <DataCard :chart-data="metricData" />
     </div>
   </div>
 </template>
