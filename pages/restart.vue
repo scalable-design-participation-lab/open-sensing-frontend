f20cb27ae91953fb8180518f73098a2ab7210e3b
DezengKong <kong.dez@northeastern.edu>
2024-10-12 05:43:52 +0400

    Implement the ImageUploadPopup placeholder

--- pages/restart.vue
+++ pages/restart.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <BackgroundMap />
-    <NavBar />
+    <!-- <BackgroundMap />
+    <NavBar /> -->
+    <ImageUploadPopup
+      :is-visible="true"
+      @close="showImageUploadPopup = false"
+      @upload="handleImageUpload"
+    />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import NavBar from '~/components/NavBar.vue'
+import { ref } from 'vue'
+
+const showImageUploadPopup = ref(false)
+
+function handleImageUpload(imageData: string) {
+  // Handle the uploaded image data
+  console.log('Image uploaded:', imageData)
+}
 </script>
