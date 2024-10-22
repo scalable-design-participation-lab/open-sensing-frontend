fbd013c7882a82a6e41935e6215aede352dc507a
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2024-10-22 13:39:31 -0400

    added firestore schema

---
+++ firestoreSchema.js
@@ -0,0 +1,180 @@
+const db = {
+    'restart-ukraine-test':
+        {
+            users: {
+                userId: {
+                    name:{lastname:'lovelace', firstname:'ada'}, 
+                    age:100, 
+                    gender:'female',
+                    'education level':'college',
+                    'resident since': '5 years',
+                    'resident near river since': '2022',
+                }
+            }
+        },
+        projects:{ 
+            projectId:{
+                userId: {
+                    timestamp:{
+                        space: {
+                            visited : {
+                                everyday:[
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ],
+                                everyweek:[
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ],
+                                sometimes:[
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ],
+                                once:[
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ],
+                                never:[
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ]
+                                }
+                            },
+                            recreational: [
+                                            {
+                                                geometry: [], 
+                                                timestamp: '2022-01-01',
+                                                comment: 'some'
+                                            }
+                                        ],
+                            restricted: [
+                                            {
+                                                geometry: [], 
+                                                timestamp: '2022-01-01',
+                                                comment: 'some'
+                                            }
+                                        ],
+                                    },
+                        belonging: {
+                            negative : [
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ],
+                            love:[
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ],
+                            positive:[
+                                    {
+                                        lat: 40.7128, 
+                                        lon: 74.0060, 
+                                        timestamp: '2022-01-01',
+                                        comment: 'some'
+                                    }
+                                ],
+                            },
+                            safety: {
+                                safe : [
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                unsafe:[
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                great:[
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                },
+                            safety: {
+                                safe : [
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                unsafe:[
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                great:[
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                },
+                            environment: {
+                                pollution : [
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                'flora-fauna':[
+                                        {
+                                            lat: 40.7128, 
+                                            lon: 74.0060, 
+                                            timestamp: '2022-01-01',
+                                            comment: 'some'
+                                        }
+                                    ],
+                                }
+                            },
+                        },
+                    }
+                }
+
+                            
+
