404ae0abc7e5a9760b19feeac378e02dabc766f8
Leonard Schrage <hello@leonardschrage.com>
2024-10-28 17:43:44 -0400

    SSR enabled

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 const { defineNuxtConfig } = require('nuxt/config')
 
 export default defineNuxtConfig({
-  ssr: false,
   css: ['vue3-openlayers/dist/vue3-openlayers.css'],
   postcss: {
     plugins: {
