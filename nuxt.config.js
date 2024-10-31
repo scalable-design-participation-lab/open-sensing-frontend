553ef96a28d77d6ed3005cb48a1ba6ef1fa89ed5
Leonard Schrage <hello@leonardschrage.com>
2024-10-31 10:00:54 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,19 +13,15 @@ export default defineNuxtConfig({
   modules: ['@pinia/nuxt', '@nuxt/ui', 'nuxt-vuefire'],
 
   vuefire: {
-    // auth: true,
     config: {
       apiKey: process.env.FIREBASE_API_KEY,
-      // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
       projectId: process.env.FIREBASE_PROJECT_ID,
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.FIREBASE_APP_ID,
       measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    },
-    // admin: {
-    //   serviceAccount: './service-account.json'
-    // }
+    }
   },
 
   components: [
