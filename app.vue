1629a85b531a83dcbbdbe06ded46624dba9f5ce6
Leonard Schrage <hello@leonardschrage.com>
2024-11-07 16:59:05 -0500

    added multi device favicon

--- app.vue
+++ app.vue
@@ -1,4 +1,17 @@
 <script>
+useHead({
+  title: 'Drawing Participation: Гуртомá',
+  meta: [
+    { name: 'apple-mobile-web-app-title', content: 'Гуртомá' },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg' },
+    { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/favicon/site.webmanifest' },
+  ],
+}),
 useSeoMeta({
   title: 'Drawing Participation: Гуртомá',
   ogTitle: 'Drawing Participation: Гуртомá',
