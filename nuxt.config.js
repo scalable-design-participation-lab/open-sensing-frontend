c165e298e8b136a0d97e5a19e8dbeafc126bfb37
Khoi Ngo <ngo.kho@northeastern.edu>
2025-05-15 14:19:59 -0400

    fix build

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,19 +19,26 @@ module.exports = defineNuxtConfig({
     dbName: process.env.DB_NAME,
     dbPassword: process.env.DB_PASSWORD,
     dbPort: process.env.DB_PORT,
-    dbURL: process.env.DB_URL,
+    dbURL: process.env.NUXT_POSTGRES_URL,
     public: {
       MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
     },
   },
 
   nitro: {
-    preset: 'cloudflare_worker',
+    preset: 'cloudflare-worker',
     compatibilityDate: '2025-05-15',
     bundler: 'esbuild',
     cloudflare: {
       nodeCompat: true, // enable Node.js polyfills (e.g. TCP sockets)
       deployConfig: true, // auto-generate wrangler config
+    },
+    externals: {
+      external: [
+        'postgres', // leave pg client out of the bundle
+        /^postgres\/.*/, // anything under postgres/*
+        'cloudflare:sockets', // leave the native sockets module external
+      ],
     },
     // rollupConfig: {
     //   // (optional) mark some imports as external to reduce bundle size
